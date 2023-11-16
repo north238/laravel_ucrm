b495e05355394646905f965d876c1a112c890ac4
north238 <fumiyama02@yahoo.co.jp>
2023-11-16 14:09:21 +0900

    vue-chart-3

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -39,9 +39,11 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('items.index')" :active="route().current('items.index')">
                                     商品管理
                                 </NavLink>
-                                <NavLink :href="route('customers.index')"
-                                    :active="route().current('customers.index')">
+                                <NavLink :href="route('customers.index')" :active="route().current('customers.index')">
                                     顧客管理
+                                </NavLink>
+                                <NavLink :href="route('analysis')" :active="route().current('analysis')">
+                                    データ分析
                                 </NavLink>
                             </div>
                         </div>
@@ -124,6 +126,9 @@ const showingNavigationDropdown = ref(false);
                         </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('customers.index')" :active="route().current('customers.index')">
                             顧客管理
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('analysis')" :active="route().current('analysis')">
+                            データ分析
                         </ResponsiveNavLink>
                     </div>
 
